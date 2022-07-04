5f519cdfea69cc12f50c24c09e16712700fd28ec
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-07-04 16:57:18 -0300

    Exercicio useState

---
+++ modulo3/use-state/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import styled from "styled-components"
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComment = styled.input `
+    width: 100%;
+    margin-right: 5px;
+`
+
+const SecaoComentario = (props) => {
+
+	const [recebeComentario, setRecebeComentario] = useState ("")
+
+	const onChangeComentario = (e) => {
+		setRecebeComentario({recebeComentario: e.target.value})
+	}
+
+	return (
+		<CommentContainer>
+			<InputComment
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={recebeComentario}
+				onChange={onChangeComentario}
+			/>
+			<button onClick={() => { props.enviarComentario() }} >Enviar</button>
+		</CommentContainer>
+	)
+}
+
+
+export default SecaoComentario
