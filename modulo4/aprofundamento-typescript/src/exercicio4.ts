93105f1b52644913ffef5a1171980099671866ab
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-08-11 15:42:18 -0300

    inicio da atividade

---
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,32 @@
+// **Exercício 4** 
+//a)
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+
+// b) Tiraria os tipos
+
+// c)
+
+// d) chamando todos de uma vez
