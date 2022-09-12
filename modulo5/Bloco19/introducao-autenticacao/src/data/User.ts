b1879a82f21303a9f3880471b52179297f94648d
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-09-12 15:22:18 -0300

    introducao-autenticacao

---
+++ modulo5/Bloco19/introducao-autenticacao/src/data/User.ts
@@ -0,0 +1,31 @@
+import { connection } from "./connection";
+
+const userTableName = "User";
+
+export const createUser = async (
+    id: string,
+    email: string,
+    password: string 
+    ) => {
+        await connection.insert({id, email, password}).into(userTableName);
+    };
+
+export const getUserByEmail = async(email:string): Promise<any> =>{
+    const result = await connection
+    .select("*")
+    .from(userTableName)
+    .where({email});
+
+    return result[0]
+}
+
+export const getUserById = async (id: string): Promise<any> => {
+    const result = await connection
+      .select("*")
+      .from(userTableName)
+      .where({ id });
+
+    return result[0];
+  }
+
+
