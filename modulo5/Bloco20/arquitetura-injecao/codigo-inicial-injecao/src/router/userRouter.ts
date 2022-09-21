0f44efe66d24995bb03d46f479535eecebe39be7
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-09-21 14:13:34 -0300

    arquitetura-injecao-completo

---
+++ modulo5/Bloco20/arquitetura-injecao/codigo-inicial-injecao/src/router/userRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { userController } from '../UserDependeces'
+
+export const userRouter = Router()
+
+
+userRouter.post("/signup", userController.signup)
+userRouter.post("/login", userController.login)
+userRouter.get("/", userController.getUsers)
+userRouter.delete("/:id", userController.deleteUser)
+userRouter.put("/:id", userController.editUser)
