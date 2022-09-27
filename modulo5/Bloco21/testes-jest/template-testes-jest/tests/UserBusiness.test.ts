65b01e2ee8e7db8c5f803c68bfe4b04db85099bb
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-09-27 12:53:01 -0300

    commit final

---
+++ modulo5/Bloco21/testes-jest/template-testes-jest/tests/UserBusiness.test.ts
@@ -0,0 +1,14 @@
+import {UserBusiness} from "../src/business/UserBusiness"
+import { AuthenticatorMock } from "./mocks/AuthenticatorMock"
+import { HashManagerMock } from "./mocks/HashManagerMock"
+import { IdGeneratorMock } from "./mocks/IdGeneratorMock"
+import { UserDatabaseMock } from "./mocks/UserDatabaseMock"
+
+describe("Testando a UserBusiness", ()=>{
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new IdGeneratorMock(),
+        new HashManagerMock(),
+        new AuthenticatorMock()
+    )
+})
